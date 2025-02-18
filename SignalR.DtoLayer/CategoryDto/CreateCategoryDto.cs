e02d129be83dc49aeae7ff33b1e68a8501cfb86e
merttguney <k.mertguney@gmail.com>
2025-02-18 20:38:03 +0300

    feat: Defined DTO's.

---
+++ SignalR.DtoLayer/CategoryDto/CreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.CategoryDto
+{
+    public class CreateCategoryDto
+    {
+        public string ProductName { get; set; }
+        public bool Status { get; set; }
+    }
+}
