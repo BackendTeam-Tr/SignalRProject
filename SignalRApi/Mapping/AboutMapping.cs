cd6426ac49db40e866692f532e0e6efa36e405f2
merttguney <k.mertguney@gmail.com>
2025-02-18 21:01:06 +0300

    feat: Added AutoMapper for DTO mapping.

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+using System.Runtime;
+
+namespace SignalRApi.Mapping
+{
+    public class AboutMapping:Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<About, ResultAboutDto>().ReverseMap(); // olası tam tersi durumda eşleşebilmeleri için.
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+           
+        }
+    }
+}
---
+++ SignalRApi/Mapping/ContactMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class ContactMapping:Profile
+    {
+        public ContactMapping()
+        {
+            CreateMap<Contact, ResultBookingDto>().ReverseMap();
+            CreateMap<Contact, CreateBookingDto>().ReverseMap();
+            CreateMap<Contact, GetBookingDto>().ReverseMap();
+            CreateMap<Contact, UpdateBookingDto>().ReverseMap();
+        }
+    }
+}
