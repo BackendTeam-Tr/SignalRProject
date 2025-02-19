6b1f1c89c22fb81520e39a178c8c748ebe985299
Mert Güney <k.mertguney@gmail.com>
2025-02-19 23:40:53 +0300

    bugfix: Corrected property naming in Category entity.

--- SignalR.EntityLayer/Entities/Category.cs
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
         public bool Status { get; set; }
     }
 }
