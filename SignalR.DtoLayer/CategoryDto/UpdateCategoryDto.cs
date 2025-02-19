9985c9b185a2a72894b3e643b8b2bb963ee992ba
Mert Güney <k.mertguney@gmail.com>
2025-02-19 23:59:39 +0300

    bugfix: Corrected property naming in UpdateCategoryDto.

--- SignalR.DtoLayer/CategoryDto/UpdateCategoryDto.cs
+++ SignalR.DtoLayer/CategoryDto/UpdateCategoryDto.cs
@@ -9,7 +9,7 @@ namespace SignalR.DtoLayer.CategoryDto
     public class UpdateCategoryDto
     {
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
         public bool Status { get; set; }
     }
 }
