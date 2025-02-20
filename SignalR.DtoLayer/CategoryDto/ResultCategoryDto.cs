e7e7d8209901c6afce7f0402da175e27ffeec63c
Mert Güney <k.mertguney@gmail.com>
2025-02-20 23:01:07 +0300

    bugfix: Corrected property naming in GetCategoryDto, ResultCategoryDto.

--- SignalR.DtoLayer/CategoryDto/ResultCategoryDto.cs
+++ SignalR.DtoLayer/CategoryDto/ResultCategoryDto.cs
@@ -9,7 +9,7 @@ namespace SignalR.DtoLayer.CategoryDto
     public class ResultCategoryDto
     {
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
         public bool Status { get; set; }
     }
 }
