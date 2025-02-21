1c73ca3bc961911b78a8a283980da2eaf2ada4c1
merttguney <k.mertguney@gmail.com>
2025-02-21 19:21:08 +0300

    bugfix: Resolve Category entity migration issue and database inconsistency.

---
+++ SignalR.DataAccessLayer/Migrations/20250221161600_Fix_CategoryTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    public partial class Fix_CategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Categories",
+                newName: "CategoryName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "ProductName");
+        }
+    }
+}
